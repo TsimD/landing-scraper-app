4ec1bc7a376b4309ec43c760184eff169b18540b
Dima Ts <dmitriitsimintiia@gmail.com>
2025-12-02 21:52:41 +0400

    config: Added @sparticuz/chromium to serverComponentsExternalPackages for Vercel deployment

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    // Включение внешних пакетов для серверных компонентов/API-маршрутов
+    serverComponentsExternalPackages: ['@sparticuz/chromium'],
+  },
 };
 
 export default nextConfig;
